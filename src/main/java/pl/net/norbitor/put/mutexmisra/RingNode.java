9811c851f4880f8f27c4b0d869533f86fe8dabf5
Norbert Langner <norbitor@gmail.com>
2018-12-29 21:20:24 +0100

    Small method cleanup; 1st node scenario implementation

--- src/main/java/pl/net/norbitor/put/mutexmisra/RingNode.java
+++ src/main/java/pl/net/norbitor/put/mutexmisra/RingNode.java
@@ -1,6 +1,5 @@
 package pl.net.norbitor.put.mutexmisra;
 
-import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.net.norbitor.put.mutexmisra.message.Message;
@@ -8,24 +7,21 @@ import pl.net.norbitor.put.mutexmisra.message.PingMessage;
 import pl.net.norbitor.put.mutexmisra.message.PongMessage;
 import pl.net.norbitor.put.mutexmisra.network.MessagePublisher;
 import pl.net.norbitor.put.mutexmisra.network.MessageSubscriber;
-import pl.net.norbitor.put.mutexmisra.util.AppUtil;
 
 public class RingNode {
 
     private final Logger logger = LoggerFactory.getLogger(RingNode.class);
 
     private int lastToken = 0;
-    private int receivedPing = 1;
-    private int receivedPong = -1;
+    private int pingNumber = 1;
+    private int pongNumber = -1;
     private int nodeId;
-    private int nodeNumber;
     private MessageSubscriber subscriber;
     private MessagePublisher publisher;
     private Thread csThread;
 
     public RingNode() {
         this.nodeId = 1;
-        this.nodeNumber = 2;
         this.subscriber = new MessageSubscriber("localhost:5555", "A", this);
         this.publisher = new MessagePublisher("*", 5555, "A");
     }
@@ -35,18 +31,26 @@ public class RingNode {
         Thread subscriberThread = new Thread(subscriber, "subscriber");
         subscriberThread.start();
         if (nodeId == 1) {
-            logger.info("This is first node, assuming token possession");
-            new Worker(this).run();
-            incarnate(receivedPing);
+            initialProcessing();
         }
+    }
+
+    private void initialProcessing() {
+        logger.info("This is first node, assuming token possession and starting CS");
+        new Worker(this).run();
+        incarnate(pingNumber);
+        PingMessage pingMessage = new PingMessage(this.pingNumber);
+        PongMessage pongMessage = new PongMessage(this.pongNumber);
+        this.publisher.sendMessage(pingMessage);
+        this.publisher.sendMessage(pongMessage);
     }
 
     public void receivePing(PingMessage msg) {
         logger.info("Ping message received (" + msg.getValue() + ") last token: " + lastToken);
-        receivedPing = msg.getValue();
+        pingNumber = msg.getValue();
         if (msg.getValue() == lastToken) {
             logger.warn("Pong message loss detected");
-            regenerate(receivedPing);
+            regenerate(pingNumber);
         } else {
             lastToken = msg.getValue();
         }
@@ -56,10 +60,10 @@ public class RingNode {
 
     public void receivePong(PongMessage msg) {
         logger.info("Pong message received (" + msg.getValue() + ") last token: " + lastToken);
-        receivedPong = msg.getValue();
+        pongNumber = msg.getValue();
         if (msg.getValue() == lastToken) {
             logger.warn("Ping message loss detected");
-            regenerate(receivedPong);
+            regenerate(pongNumber);
         } else {
             lastToken = msg.getValue();
         }
@@ -73,35 +77,18 @@ public class RingNode {
 
     public void leaveCS() {
         logger.info("Leaving Critical Section");
-        incarnate(receivedPing);
+        incarnate(pingNumber);
     }
 
     private void regenerate(int value) {
         logger.info("Regenerating Ping and Pong messages");
-        PingMessage pingMessage = new PingMessage(Math.abs(value));
-        PongMessage pongMessage = AppUtil.getPongFromPing(pingMessage);
-        publishMessage(pingMessage);
-        publishMessage(pongMessage);
+        this.pingNumber = Math.abs(value);
+        this.pongNumber = -this.pingNumber;
     }
 
     private void incarnate(int value) {
         logger.info("Incarnating Ping and Pong messages");
-        PingMessage pingMessage = new PingMessage(Math.abs((value + 1)));
-        PongMessage pongMessage = AppUtil.getPongFromPing(pingMessage);
-        int loss = RandomUtils.nextInt(0,4);
-        if (loss != 3) {
-            publishMessage(pingMessage);
-        } else {
-            logger.warn("Will loss PING");
-        }
-        if (RandomUtils.nextInt(0,4) != 3 || loss == 3) {
-            publishMessage(pongMessage);
-        } else {
-            logger.warn("Will loss PONG");
-        }
-    }
-
-    private void publishMessage(Message message) {
-        publisher.sendMessage(message);
+        this.pingNumber = Math.abs(value) + 1;
+        this.pongNumber = -this.pingNumber;
     }
 }
